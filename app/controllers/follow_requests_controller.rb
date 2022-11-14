c22fc6981e23555dbb980b30e0b08b91cbff63c9
petersjang1993 <petersjang1993@gmail.com>
2022-11-14 20:16:25 +0000

    making progress with follow request

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -3,7 +3,7 @@ class FollowRequestsController < ApplicationController
     the_follow_request = FollowRequest.new
     the_follow_request.recipient_id = params.fetch("query_recipient_id")
     the_follow_request.sender_id = params.fetch("query_sender_id")
-    the_follow_request.status = params.fetch("query_status")
+    the_follow_request.status = "pending"
 
     if the_follow_request.valid? 
       the_follow_request.save
@@ -28,7 +28,7 @@ class FollowRequestsController < ApplicationController
 
   def delete 
     the_id = params.fetch("path_id")
-    the_request = FollowRequest.where({ :id => the_id })
+    the_request = FollowRequest.where({ :id => the_id }).at(0)
 
     the_request.destroy 
 
