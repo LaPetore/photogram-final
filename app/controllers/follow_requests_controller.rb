044519183e12f9969fe6e706a6ce7b83bcaa912a
petersjang1993 <petersjang1993@gmail.com>
2022-11-15 03:35:58 +0000

    follower count done

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -3,7 +3,9 @@ class FollowRequestsController < ApplicationController
     the_follow_request = FollowRequest.new
     the_follow_request.recipient_id = params.fetch("query_recipient_id")
     the_follow_request.sender_id = params.fetch("query_sender_id")
+
     the_follow_request.status = "pending"
+    
 
     if the_follow_request.valid? 
       the_follow_request.save
